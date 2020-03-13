839d5ba13e6a891048e5cf32043661326ba650a7
dongqianrui <1215217867@qq.com>
2020-03-13 23:50:52 +0800

    Create Reptile14_1_DHTML.py

---
+++ Reptile14_1_DHTML.py
@@ -0,0 +1,15 @@
+"""
+通过webdriver操作模拟进行查找
+"""
+from selenium import webdriver
+import time
+#通过keys模拟键盘
+from selenium.webdriver.common.keys import Keys
+#操作哪个浏览器就对哪个浏览器建立一个实例
+#自动按照环境变量查找相应的浏览器
+driver = webdriver.PhantomJS()#这个就是浏览器的实例
+#如果浏览器没有相应的环境浏览器，需要指定浏览器位置
+driver.get("http://www.baidu.com")#去访问这个网站，然后获取返回的数据
+#通过函数查找title标签
+print("Title:{0}".format(driver.title))
+
